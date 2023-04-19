9dd01588a285633198aeef10817fd30680321ce5
Sebastian <sebastiansloma@gmail.com>
2023-04-19 15:52:11 +0200

    added nav

---
+++ script.js
@@ -0,0 +1,8 @@
+// NAVIGATION
+const toggleMenu = () =>{
+    const toggleMenu = document.querySelector('.toggleMenu')
+    const navigation = document.querySelector('.navigation')
+    toggleMenu.classList.toggle('active')
+    navigation.classList.toggle('active')
+}
+// END NAVIGATION
