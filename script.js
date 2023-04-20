1af654b23c72125c3389bbd2027541bf391390e5
Sebastian <sebastiansloma@gmail.com>
2023-04-20 13:09:43 +0200

    added gallery

--- script.js
+++ script.js
@@ -6,3 +6,22 @@ const toggleMenu = () =>{
     navigation.classList.toggle('active')
 }
 // END NAVIGATION
+
+// GALLERY
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 10,
+    slidesPerView: 4,
+    freeMode: true,
+    watchSlidesProgress: true,
+  });
+  var swiper2 = new Swiper(".mySwiper2", {
+    spaceBetween: 10,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    thumbs: {
+      swiper: swiper,
+    },
+  });
+// END GALLERY
