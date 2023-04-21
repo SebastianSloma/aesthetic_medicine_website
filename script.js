a850c8cda6ed262ea4816c01945945c0187a3486
Sebastian <sebastiansloma@gmail.com>
2023-04-21 07:33:16 +0200

    .

--- script.js
+++ script.js
@@ -1,27 +1,13 @@
 // NAVIGATION
-const toggleMenu = () =>{
-    const toggleMenu = document.querySelector('.toggleMenu')
-    const navigation = document.querySelector('.navigation')
-    toggleMenu.classList.toggle('active')
-    navigation.classList.toggle('active')
-}
+const toggleMenu = () => {
+	const toggleMenu = document.querySelector('.toggleMenu');
+	const navigation = document.querySelector('.navigation');
+	toggleMenu.classList.toggle('active');
+	navigation.classList.toggle('active');
+};
 // END NAVIGATION
 
 // GALLERY
-var swiper = new Swiper(".mySwiper", {
-    spaceBetween: 10,
-    slidesPerView: 4,
-    freeMode: true,
-    watchSlidesProgress: true,
-  });
-  var swiper2 = new Swiper(".mySwiper2", {
-    spaceBetween: 10,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    thumbs: {
-      swiper: swiper,
-    },
-  });
+
+
 // END GALLERY
