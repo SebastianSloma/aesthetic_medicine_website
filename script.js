73f85d6eea7f3a136bac8b4101015dd88bd876f2
Sebastian <sebastiansloma@gmail.com>
2023-04-21 09:03:52 +0200

    added gallery

--- script.js
+++ script.js
@@ -8,6 +8,36 @@ const toggleMenu = () => {
 // END NAVIGATION
 
 // GALLERY
-
+const swiper = new Swiper('.gallery-slider', {
+	// Optional parameters
+	slidesPerView:1,
+	spaceBetween: 70,
+  
+	// If we need pagination
+	pagination: {
+	  el: '.swiper-pagination',
+	  clickable: true,
+	},
+  
+	// Navigation arrows
+	navigation: {
+	  nextEl: '.gallery-slider .swiper-button-next',
+	  prevEl: '.gallery-slider .swiper-button-prev',
+	},
+	breakpoints: {
+		640: {
+			slidesPerView: 2,
+		},
+		950:{
+			slidesPerView: 3,
+		},
+		1200: {
+			slidesPerView: 4
+		}
+	}
+  
+	
+  });
+ 
 
 // END GALLERY
