0793dfc51b31ba0b09ed0aad4f7e976f605d75c7
Sebastian <sebastiansloma@gmail.com>
2023-04-26 16:45:12 +0200

    added treatments section

--- script.js
+++ script.js
@@ -10,30 +10,47 @@ const toggleMenu = () => {
 // GALLERY
 const swiper = new Swiper('.gallery-slider', {
 	// Optional parameters
-	slidesPerView:1,
+	slidesPerView: 1,
 	spaceBetween: 70,
-  
 
-  
 	// Navigation arrows
 	navigation: {
-	  nextEl: '.gallery-slider .swiper-button-next',
-	  prevEl: '.gallery-slider .swiper-button-prev',
+		nextEl: '.gallery-slider .swiper-button-next',
+		prevEl: '.gallery-slider .swiper-button-prev',
 	},
 	breakpoints: {
 		640: {
 			slidesPerView: 2,
 		},
-		950:{
+		950: {
 			slidesPerView: 3,
 		},
 		1200: {
-			slidesPerView: 4
-		}
-	}
-  
-	
-  });
- 
+			slidesPerView: 4,
+		},
+	},
+});
 
 // END GALLERY
+
+// treatments
+const swiper2 = new Swiper('.treatments-slider', {
+	slidesPerView: 1,
+	spaceBetween: 10,
+	loop: true,
+	navigation: {
+		nextEl: '.treatments-slider .swiper-button-next',
+		prevEl: '.treatments-slider .swiper-button-prev',
+	},
+	breakpoints: {
+		640: {
+			slidesPerView: 2,
+			spaceBetween:20,
+		},
+		1024: {
+			slidesPerView: 3,
+			spaceBetween: 50,
+		},
+	},
+});
+// end treatments
